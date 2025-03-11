472a78da31df7325ee1d3513c87b10accc195a29
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-03-11 19:32:44 +0530

    Time: 2 ms (84.69%), Space: 45.7 MB (95.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,28 @@
 class Solution {
+    // public void moveZeroes(int[] nums) {
+    //     int end = nums.length-1;
+    //     int start = 0;
+    //     int temp = 0;
+    //     for(int i=0;i<nums.length;i++){
+    //         if(nums[i]!=0){
+    //            nums[start++]=nums[i];
+    //         }else{
+    //             temp++;
+    //         }
+    //     }
+    //     Arrays.fill(nums,nums.length-temp,nums.length,0);
+    // }
     public void moveZeroes(int[] nums) {
-        int end = nums.length-1;
-        int start = 0;
-        int temp = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-               nums[start++]=nums[i];
-            }else{
-                temp++;
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
             }
         }
-        Arrays.fill(nums,nums.length-temp,nums.length,0);
     }
 }
