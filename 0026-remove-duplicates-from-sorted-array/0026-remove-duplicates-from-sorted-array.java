ec2793dac66b62310151c7925378f2e111ae39ae
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-03-11 19:20:17 +0530

    Time: 1 ms (66.46%), Space: 44.7 MB (89.87%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,18 +1,29 @@
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         // int index = 0;
+//         // for(int i = 0;i<nums.length;i++){
+//         //     if(nums[index]!=nums[i]){
+//         //         nums[++index]=nums[i];
+//         //     }
+//         // }
+//         // return index+1;
+//         int k=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[k]!=nums[i]){
+//                 nums[++k]=nums[i];      
+//             }
+//         } 
+//         return k+1;
+//     }
+// }
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // int index = 0;
-        // for(int i = 0;i<nums.length;i++){
-        //     if(nums[index]!=nums[i]){
-        //         nums[++index]=nums[i];
-        //     }
-        // }
-        // return index+1;
-        int k=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[k]!=nums[i]){
-                nums[++k]=nums[i];      
-            }
-        } 
-        return k+1;
+      int k=0;
+      for(int i=0;i<nums.length;i++){
+        if(nums[k]!=nums[i]){
+            nums[++k]=nums[i];      
+        }
+      } 
+     return k+1;
     }
 }
