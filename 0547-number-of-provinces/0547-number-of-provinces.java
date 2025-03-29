25399130ad0d2960ff172137a1b379bae39b6d78
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-03-29 15:45:09 +0530

    Time: 1 ms (88.91%), Space: 47.7 MB (63.2%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,37 +1,58 @@
+//class Solution {
+    // public int findCircleNum(int[][] isConnected) {
+    //     int count = 0;
+    //     int V = isConnected.length;
+    //     ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+    //     for(int i = 0;i<V;i++){
+    //         adj.add(new ArrayList<Integer>());
+    //     }
+    //     for(int i = 0;i<V;i++){
+    //         for(int j =0;j<V;j++){
+    //             if(isConnected[i][j]==1 && i!=j){
+    //                 adj.get(i).add(j);
+    //                 adj.get(j).add(i);
+    //             }
+    //         }
+    //     }
+    //     int[] vis = new int[V+1];
+    //     for(int i = 0;i<V;i++){
+    //         if(vis[i]==0){
+    //             count++;
+    //             DFS(i,vis,adj);
+    //         }
+    //     }
+    //     return count;
+
+    // }
+    // private static void DFS(int node,int[] vis, ArrayList<ArrayList<Integer>> adj){
+    //     vis[node] = 1;
+    //     for(Integer i: adj.get(node)){
+    //         if(vis[i]==0){
+    //             DFS(i,vis,adj);
+    //         }
+    //     }
+    // }
+//}    
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int count = 0;
-        int V = isConnected.length;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-        for(int i = 0;i<V;i++){
-            adj.add(new ArrayList<Integer>());
-        }
-        for(int i = 0;i<V;i++){
-            for(int j =0;j<V;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
+        int cities = isConnected.length;
+        int provinces = 0;
+        int[] vis = new int[cities+1];
+        for(int i = 1; i <= cities; i++) {
+            if(vis[i] == 0) {
+                provinces++;
+                dfs(i, vis, isConnected);
             }
         }
-        int[] vis = new int[V+1];
-        for(int i = 0;i<V;i++){
-            if(vis[i]==0){
-                count++;
-                DFS(i,vis,adj);
-            }
-        }
-        return count;
-
+        return provinces;
     }
-    private static void DFS(int node,int[] vis, ArrayList<ArrayList<Integer>> adj){
+
+    private void dfs(int node, int[] vis, int[][] isConnected) {
         vis[node] = 1;
-        for(Integer i: adj.get(node)){
-            if(vis[i]==0){
-                DFS(i,vis,adj);
-            }
+        for (int i = 1; i <= isConnected.length; i++) {
+            if(isConnected[node-1][i-1] == 1 && vis[i] == 0) {
+                dfs(i, vis, isConnected);
+            } 
         }
     }
-
-    
 }
