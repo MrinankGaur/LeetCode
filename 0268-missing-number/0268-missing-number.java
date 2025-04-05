aaa94e0eda6437e8ec401d4a73c43a1b676c6b14
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-04-05 19:17:43 +0530

    Time: 0 ms (100%), Space: 46 MB (7.46%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,12 @@
 class Solution {
      public int missingNumber(int[] nums) {
-        boolean[] arr = new boolean[nums.length+1];
-        Arrays.fill(arr, false);
-        for (int i = 0; i < nums.length; i++) {
-            arr[nums[i]] = true;
+        int sum = 0;
+        int max = 0;
+        for(int i = 0;i<nums.length;i++){
+            max+=i;
+            sum+=nums[i];
         }
-        int i=0;
-        for (i = 0; i < arr.length; i++) {
-            if(arr[i]==false)
-                break;
-        }
-        return i;
+        max+=nums.length;
+        return max-sum;
     }
 }
