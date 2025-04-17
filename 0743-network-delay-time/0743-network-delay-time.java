7b0994ae608eba48fa0a200c283cfeb878826759
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-04-17 17:36:21 +0530

    Time: 10 ms (85.82%), Space: 49.1 MB (25.17%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,34 +1,49 @@
+class Pair {
+    int first; //node
+    int second; //edge weight or distance
+    Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
 class Solution {
-    public int networkDelayTime(int[][] times, int N, int K) {
-        Map<Integer, Map<Integer,Integer>> map = new HashMap<>();
-        for(int[] time : times){
-            map.putIfAbsent(time[0], new HashMap<>());
-            map.get(time[0]).put(time[1], time[2]);
+    public int networkDelayTime(int[][] times, int n, int k) {
+        //k is basically the source node then
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++) {
+            adj.add(new ArrayList<>());
         }
-        
-        //distance, node into pq
-        Queue<int[]> pq = new PriorityQueue<>((a,b) -> (a[0] - b[0]));
-        
-        pq.add(new int[]{0, K});
-        
-        boolean[] visited = new boolean[N+1];
-        int res = 0;
-        
-        while(!pq.isEmpty()){
-            int[] cur = pq.remove();
-            int curNode = cur[1];
-            int curDist = cur[0];
-            if(visited[curNode]) continue;
-            visited[curNode] = true;
-            res = curDist;
-            N--;
-            if(map.containsKey(curNode)){
-                for(int next : map.get(curNode).keySet()){
-                    pq.add(new int[]{curDist + map.get(curNode).get(next), next});
+        for(int time[]: times) {
+            int u = time[0];
+            int v = time[1];
+            int w = time[2];
+            adj.get(u-1).add(new int[]{v-1, w});
+        }
+        int []dist = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[k-1]=0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b)-> a.second - b.second);
+        pq.add(new Pair(k-1, 0));
+        while(!pq.isEmpty()) {
+            Pair top = pq.poll();
+            int dis = top.second;
+            int node = top.first;
+            for(int neighbour[] : adj.get(node)) {
+                int adjNode = neighbour[0];
+                int edW = neighbour[1];
+                if(dis+edW<dist[adjNode]) {
+                    dist[adjNode]=dis+edW;
+                    pq.add(new Pair(adjNode, dis+edW));
                 }
             }
         }
-        return N == 0 ? res : -1;
-            
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            if(dist[i]==Integer.MAX_VALUE) {
+                    return -1;
+            }
+            ans=Math.max(ans, dist[i]);
+        }
+        return ans;
     }
 }
