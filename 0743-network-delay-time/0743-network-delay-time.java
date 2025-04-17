ef54409f481abbca50af268c2c652a0988561c87
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-04-17 17:36:29 +0530

    Time: 10 ms (85.82%), Space: 49.1 MB (25.17%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,49 +1,50 @@
-class Pair {
-    int first; //node
-    int second; //edge weight or distance
-    Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        //k is basically the source node then
-        List<List<int[]>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) {
+        int row = times.length;
+        int col = times[0].length;
+        int t = 0;
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for(int i = 0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int time[]: times) {
-            int u = time[0];
-            int v = time[1];
-            int w = time[2];
-            adj.get(u-1).add(new int[]{v-1, w});
+        for(int[] temp:times){
+            int src = temp[0];
+            int target = temp[1];
+            int time = temp[2];
+            adj.get(src-1).add(new int[]{target-1,time});
         }
-        int []dist = new int[n];
+        int[] dist = new int[n];
         Arrays.fill(dist,Integer.MAX_VALUE);
         dist[k-1]=0;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b)-> a.second - b.second);
-        pq.add(new Pair(k-1, 0));
-        while(!pq.isEmpty()) {
-            Pair top = pq.poll();
-            int dis = top.second;
-            int node = top.first;
-            for(int neighbour[] : adj.get(node)) {
+        PriorityQueue<Pair> q = new PriorityQueue<>((x,y)->x.first-y.first);
+        q.add(new Pair(0,k-1));
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            int node = curr.second;
+            int time = curr.first;
+            for(int[] neighbour: adj.get(node)){
                 int adjNode = neighbour[0];
                 int edW = neighbour[1];
-                if(dis+edW<dist[adjNode]) {
-                    dist[adjNode]=dis+edW;
-                    pq.add(new Pair(adjNode, dis+edW));
+                if((time+edW)<dist[adjNode]){
+                    dist[adjNode] = time+edW;
+                    q.add(new Pair(dist[adjNode],adjNode));
                 }
             }
+            
         }
-        int ans = 0;
-        for(int i=0;i<n;i++) {
-            if(dist[i]==Integer.MAX_VALUE) {
-                    return -1;
-            }
-            ans=Math.max(ans, dist[i]);
+        for(int i:dist){
+            if(i==Integer.MAX_VALUE) return -1;
+            t=Math.max(t,i);
         }
-        return ans;
+        return t;
+
+
+    }
+    class Pair{
+        int first,second;
+        public Pair(int first, int second){
+            this.first = first;//time
+            this.second = second;//node
+        }
     }
 }
