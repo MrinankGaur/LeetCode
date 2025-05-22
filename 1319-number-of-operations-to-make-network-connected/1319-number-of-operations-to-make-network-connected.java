3349868d35a062780891b24b48fbde59341d578d
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-05-22 10:47:52 +0530

    Time: 5 ms (77.85%), Space: 61.6 MB (51.87%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,79 +1,68 @@
 class Solution {
+    class DSU {
+        int [] parent;
+        int [] size;
+        int noOfComponents;
+
+        public DSU(int n){
+            noOfComponents = n;
+            parent = new int [n];
+            size = new int [n];
+            for(int i=0; i<n; i++){
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public boolean union(int n1, int n2){
+            int p1 = find(n1);
+            int p2 = find(n2);
+
+            if(p1 == p2){
+                return false;
+            }
+
+            // merge needs to happen
+            if(size[p1] > size[p2]){
+                parent[p2] = p1;
+                size[p1] += size[p2];
+            } else {
+                parent[p1] = p2;
+                size[p2] += size[p1];
+            }
+            noOfComponents--;
+
+            return true;
+        }
+
+        public int find(int n){
+            int p = parent[n];
+            if(p != n){
+                p = find(p);
+                parent[n] = p;
+            }
+            return p;
+        }
+    }
+
     public int makeConnected(int n, int[][] connections) {
-        if((connections.length)<n-1){
-            return -1;
-        }
-        DisjointSet ds = new DisjointSet(n);
-        int extraEdges=0;
-        int nc = 0;
-        for(int[] arr: connections){
-            int u = arr[0];
-            int v = arr[1];
-            if(ds.findUParent(u)!=ds.findUParent(v)){
-                ds.unionBySize(u,v);
-            }
-            else{
-                extraEdges++;
+        DSU dsu = new DSU(n);
+        int extraCable = 0;
+
+        for(int [] connection : connections){
+            if(!dsu.union(connection[0], connection[1])){
+                extraCable++;
             }
         }
-         Set<Integer> uniqueParents = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            uniqueParents.add(ds.findUParent(i));
+
+
+        int components = 0;
+        for(int i=0; i<dsu.parent.length; i++){
+            if(dsu.parent[i] == i){
+                components++;
+            }
         }
 
-        int components = uniqueParents.size();
-        int required = components - 1;
-
-        return extraEdges >= required ? required : -1;
-        
-    }
-}
-public class DisjointSet {
-    List<Integer> rank = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    List<Integer> parent = new ArrayList<>();
-    public DisjointSet(int n){
-        for(int i =0;i<n;i++){
-            rank.add(0);
-            parent.add(i);
-            size.add(1);
-        }
-    }
-    public int findUParent(int node){
-        if(parent.get(node)==node){
-            return node;
-        }
-        int ulp = findUParent(parent.get(node));
-        parent.set(node,ulp);
-        return parent.get(node);
-    }
-    public void unionByRank(int u,int v){
-        int ulp_u = findUParent(u);
-        int ulp_v = findUParent(v);
-        if(ulp_u==ulp_v)return;
-        if(rank.get(ulp_u)>rank.get(ulp_v)){
-            parent.set(ulp_v,ulp_u);
-        }
-        else if(rank.get(ulp_v)>rank.get(ulp_u)){
-            parent.set(ulp_u,ulp_v);
-        }
-        else{
-            parent.set(ulp_u,ulp_v);
-            int RankU = rank.get(ulp_u);
-            rank.set(ulp_u,RankU+1);
-        }
-    }
-    public void unionBySize(int u,int v) {
-        int ulp_u = findUParent(u);
-        int ulp_v = findUParent(v);
-        if(ulp_u==ulp_v)return;
-        if(size.get(ulp_u)<size.get(ulp_v)){
-            parent.set(ulp_u,ulp_v);
-            size.set(ulp_v,size.get(ulp_v)+size.get(ulp_u));
-        }
-        else{
-            parent.set(ulp_v,ulp_u);
-            size.set(ulp_u,size.get(ulp_v)+size.get(ulp_u));
-        }
+        return extraCable >= components - 1 ? components - 1 : -1;
     }
 }
