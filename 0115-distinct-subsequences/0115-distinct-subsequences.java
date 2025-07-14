83611cbe82a51354b3fe4fa5c631d6fd7f1ada40
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-07-14 17:11:13 +0530

    Time: 16 ms (88.38%), Space: 45 MB (91.5%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n1 = s.length();
+        int n2 = t.length();
+
+        int[] prev = new int[n2 + 1];
+        prev[0] = 1;
+
+        for (int i = 1; i <= n1; i++) {
+            int[] curr = new int[n2 + 1];
+            curr[0] = 1;
+
+            for (int j = 1; j <= n2; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    curr[j] = prev[j - 1] + prev[j];
+                } else {
+                    curr[j] = prev[j];
+                }
+            }
+
+            prev = curr;
+        }
+
+        return prev[n2];
+    }
+}
