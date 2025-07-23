9b91c69518fd203021158f56973bf5e90944812c
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-07-23 16:44:35 +0530

    Time: 10 ms (98.24%), Space: 42.5 MB (71.56%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -8,10 +8,21 @@ class Solution {
             arr[i+1] = cuts[i];
         }
         int[][] dp = new int[len+2][len+2];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        // for(int[] row:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        // return f(1,len,arr,dp);
+        for(int i=len;i>=1;i--){
+            for(int j = i;j<=len;j++){
+              int min = Integer.MAX_VALUE;
+                for(int k = i;k<=j;k++){
+                    int cost = arr[j+1] - arr[i-1] + dp[i][k-1] + dp[k+1][j];
+                    min = Math.min(cost,min);
+                }
+                dp[i][j] = min;  
+            }
         }
-        return f(1,len,arr,dp);
+        return dp[1][len];
         
     }
     public static int f(int i,int j,int[] cuts,int[][] dp){
