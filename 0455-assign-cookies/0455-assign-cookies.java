acdf0830737ac6d4f69be8cf124f945b8a41e098
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-07-30 19:20:39 +0530

    Time: 9 ms (98.84%), Space: 48.7 MB (46.16%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i =0;
+        int j=0;
+        while(j<g.length && i<s.length){
+            if(s[i]>=g[j]){
+                j++;
+            }
+            i++;
+        }
+        return j;
+    }
+}
