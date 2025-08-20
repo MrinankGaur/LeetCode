cfba070d3b59bd191802489eaaabc55099fce3c7
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-20 19:14:31 +0530

    Time: 9 ms (93.06%), Space: 49.2 MB (30.33%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,50 +1,48 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int row = times.length;
-        int col = times[0].length;
-        int t = 0;
-        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
         for(int i = 0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] temp:times){
-            int src = temp[0];
-            int target = temp[1];
-            int time = temp[2];
-            adj.get(src-1).add(new int[]{target-1,time});
+        for(int[] arr:times){
+            int u = arr[0]-1;
+            int v = arr[1]-1;
+            int d = arr[2];
+            adj.get(u).add(new Pair(v,d));
         }
+        int min = Integer.MIN_VALUE;
         int[] dist = new int[n];
         Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[k-1]=0;
-        PriorityQueue<Pair> q = new PriorityQueue<>((x,y)->x.first-y.first);
-        q.add(new Pair(0,k-1));
-        while(!q.isEmpty()){
-            Pair curr = q.poll();
-            int node = curr.second;
-            int time = curr.first;
-            for(int[] neighbour: adj.get(node)){
-                int adjNode = neighbour[0];
-                int edW = neighbour[1];
-                if((time+edW)<dist[adjNode]){
-                    dist[adjNode] = time+edW;
-                    q.add(new Pair(dist[adjNode],adjNode));
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y)->Integer.compare(x.second,y.second));
+        pq.offer(new Pair(k-1,0));
+        dist[k-1] = 0;
+        while(!pq.isEmpty()){
+            Pair curr = pq.poll();
+            int node = curr.first;
+            int time = curr.second;
+            if(time>dist[node]) continue;
+            for(Pair it: adj.get(node)){
+                int next = it.first;
+                int t = time + it.second;
+                if(t<dist[next]){
+                    dist[next] = t;
+                    pq.offer(new Pair(next,t));
                 }
             }
-            
         }
-        for(int i:dist){
-            if(i==Integer.MAX_VALUE) return -1;
-            t=Math.max(t,i);
+        int ans = 0;
+        for(int d:dist){
+            if(d==Integer.MAX_VALUE) return -1;
+            ans = Math.max(d,ans);
         }
-        return t;
-
-
+        return ans;
     }
-    class Pair{
-        int first,second;
-        public Pair(int first, int second){
-            this.first = first;//time
-            this.second = second;//node
-        }
+}
+
+class Pair{
+    int first,second;
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
     }
 }
