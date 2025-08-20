b9232c03c97217abb54da218b1c552d034a602d4
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-20 14:49:37 +0530

    Time: 1 ms (64.28%), Space: 42.7 MB (7.1%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,42 +15,26 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root==null){
-            return ans;
-        }
-        Deque<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        boolean zigzag = false;
-        while(!queue.isEmpty()){
-            int n = queue.size();
-            List<Integer> list = new ArrayList<>();
-            for(int i =0;i<n;i++){
-                
-                if(!zigzag){
-                    TreeNode currentNode = queue.pollFirst();
-                    list.add(currentNode.val); 
-                    if(currentNode.left != null){
-                        queue.offerLast(currentNode.left);
-                    }
-                    if(currentNode.right != null){
-                        queue.offerLast(currentNode.right);
-                    }            
+        List<List<Integer>> list = new ArrayList<>();
+        int level = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root!=null) q.offer(root);
+        while(!q.isEmpty()){
+            List<Integer> arr = new ArrayList<>();
+            int size = q.size();
+            for(int i = 0;i<size;i++){
+                TreeNode curr = q.poll();
+                if(level%2==0){
+                    arr.add(curr.val);
                 }else{
-                    TreeNode currentNode = queue.pollLast();
-                    list.add(currentNode.val); 
-                    if(currentNode.right != null){
-                        queue.offerFirst(currentNode.right);
-                    }
-                    if(currentNode.left != null){
-                        queue.offerFirst(currentNode.left);
-                    } 
+                    arr.addFirst(curr.val);
                 }
-                
+                if(curr.left!=null) q.offer(curr.left);
+                if(curr.right!=null) q.offer(curr.right);
             }
-            zigzag = !zigzag;
-            ans.add(list);
+            level++;
+            list.add(arr);
         }
-        return ans;
+        return list;
     }
 }
