62da32bfa8e45935ee9c87e5862f514b1cb8aa76
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-21 13:29:57 +0530

    Time: 87 ms (26.52%), Space: 57.8 MB (18.84%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,38 +1,29 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int i : nums){
-            sum+=i;
+        for(int it: nums){
+            sum+=it;
         }
-        if(sum%2!=0){
+        int target=sum/2;
+        int n = nums.length;
+        int[][] dp = new int[n][target+1];
+        for(int[] row: dp) Arrays.fill(row,-1);
+        if(sum%2!=0) return false;
+        return f(nums.length-1,target,nums,dp);
+        
+    }
+    public boolean f(int i,int target,int[] nums, int[][] dp){
+        if(i==0){
+            if(target==0){
+                return true;
+            }
             return false;
         }
-        else{
-            int target = sum/2;
-            int[][] dp = new int[nums.length][target+1];
-            for(int[] row: dp){
-                Arrays.fill(row,-1);
-            }
-            return helper(nums.length-1,target,nums,dp);
-        }
-    }
-    static Boolean helper(int index, int target,int[] arr,int[][] dp){
-        if(target==0) 
-            return true;
-        if(index==0) 
-            return arr[0]==target;
-        if(dp[index][target]!=-1) 
-            return (dp[index][target]==1);
-        
-        boolean notTake = helper(index-1,target,arr,dp);
+        if(dp[i][target]!=-1) return dp[i][target]==1;
+        boolean notTake = f(i-1,target,nums,dp);
         boolean take = false;
-        if(target>=arr[index]){
-            take = helper(index-1,target-arr[index],arr,dp);
-        }
-        
-        dp[index][target] = (take || notTake) ? 1 : 0;
-        
+        if(nums[i]<=target) take = f(i-1,target-nums[i],nums,dp);
+        dp[i][target] = take || notTake ? 1 : 0;
         return take || notTake;
-        
     }
 }
