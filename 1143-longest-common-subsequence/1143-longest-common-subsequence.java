68501ad57d89f9c1bf201a6a8c7d1ecc9cfe0793
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-21 14:16:13 +0530

    Time: 20 ms (72.91%), Space: 50.8 MB (76.99%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,34 +1,29 @@
 class Solution {
-    public int longestCommonSubsequence(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        // int[][] dp = new int[n+1][m+1];
-        // for(int[] row:dp){
-        //     Arrays.fill(row,-1);
-        // }
-        // return f(n,m,s1,s2,dp);
-        int[] prev = new int[m+1];
-        int[] curr = new int[m+1];
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n+1][m+1];
+        // for(int[] row:dp) Arrays.fill(row, -1);
+        // return lcs(i,j,text1,text2,dp);
         for(int i = 1;i<=n;i++){
             for(int j = 1;j<=m;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    curr[j] = 1 + prev[j-1];
-                }else{
-                    curr[j]=Math.max(prev[j],curr[j-1]);
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j] = 1+ dp[i-1][j-1];
                 }
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
             }
-            prev = curr.clone();
         }
-        return prev[m];
+        return dp[n][m];
     }
-    public int f(int i,int j,String s1,String s2,int[][]dp){
+    public int lcs(int i,int j,String s1,String s2,int[][] dp){
         if(i==0 || j==0){
             return 0;
         }
         if(dp[i][j]!=-1) return dp[i][j];
         if(s1.charAt(i-1)==s2.charAt(j-1)){
-            return dp[i][j] = 1 + f(i-1,j-1,s1,s2,dp);
+            return dp[i][j] = 1+lcs(i-1,j-1,s1,s2,dp);
         }
-        return dp[i][j]=Math.max(f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp));
+        return dp[i][j] = Math.max(lcs(i-1,j,s1,s2,dp),lcs(i,j-1,s1,s2,dp));
+
     }
 }
