1ee8cabbcbca424c47b983c5999f9bfcd64c300b
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-22 15:14:17 +0530

    Time: 7 ms (44.45%), Space: 45.3 MB (68.56%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,29 @@
 class Solution {
-    public boolean canFinish(int n, int[][] preq) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+    public boolean canFinish(int numCourses, int[][] prereq) {
+        ArrayList<ArrayList<Integer>> adj =new ArrayList<>();
+         int[] indeg = new int[numCourses];
+        for(int i = 0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-
-        int ans = 0;
-        int[] indeg = new int[n];
-
-        for (int[] arr : preq) {
-            int course = arr[0];
-            int pre = arr[1];
-            adj.get(pre).add(course);
-            indeg[course]++;
+        for(int[] arr:prereq){
+            adj.get(arr[0]).add(arr[1]);
+            indeg[arr[1]]++;
         }
-
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indeg[i] == 0) {
-                q.offer(i);
-            }
+        for(int i =0;i<numCourses;i++){
+            if(indeg[i]==0) q.offer(i);
         }
-
-        while (!q.isEmpty()) {
+        int topo = 0;
+        while(!q.isEmpty()){
             int curr = q.poll();
-            ans++;
-            for (int next : adj.get(curr)) {
-                indeg[next]--;
-                if (indeg[next] == 0) {
-                    q.offer(next);
+            topo++;
+            for(Integer it:adj.get(curr)){
+                indeg[it]--;
+                if(indeg[it]==0){
+                    q.offer(it);
                 }
             }
         }
-
-        return ans == n;
+        return topo==numCourses;
     }
 }
