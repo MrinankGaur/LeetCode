cc63447e82a29265ef6e3695b8fc3e64645df0ce
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-22 15:10:14 +0530

    Time: 7 ms (44.45%), Space: 46.2 MB (8.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,40 +1,38 @@
 class Solution {
-    public boolean canFinish(int n, int[][] prerequisites) {
-        List<Integer>[] adj = new List[n];
-        int[] indegree = new int[n];
-        List<Integer> ans = new ArrayList<>();
-
-        for (int[] pair : prerequisites) {
-            int course = pair[0];
-            int prerequisite = pair[1];
-            if (adj[prerequisite] == null) {
-                adj[prerequisite] = new ArrayList<>();
-            }
-            adj[prerequisite].add(course);
-            indegree[course]++;
+    public boolean canFinish(int numCourses, int[][] prereq) {
+        ArrayList<ArrayList<Integer>> adj =new ArrayList<>();
+        for(int i = 0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
         }
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
+        for(int[] arr:prereq){
+            int u = arr[0];
+            int v = arr[1];
+            adj.get(u).add(v);
+        }
+        int[] indeg = new int[numCourses];
+        for(int i =0;i<numCourses;i++){
+            for(int it:adj.get(i)){
+                indeg[it]++;
             }
         }
-
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            ans.add(current);
-
-            if (adj[current] != null) {
-                for (int next : adj[current]) {
-                    indegree[next]--;
-                    if (indegree[next] == 0) {
-                        queue.offer(next);
-                    }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i =0;i<numCourses;i++){
+            if(indeg[i]==0) q.offer(i);
+        }
+        ArrayList<Integer> topo = new ArrayList<>();
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            topo.add(curr);
+            for(Integer it:adj.get(curr)){
+                indeg[it]--;
+                if(indeg[it]==0){
+                    q.offer(it);
                 }
             }
         }
+        return topo.size()==numCourses;
 
-        return ans.size() == n;
+
+
     }
 }
