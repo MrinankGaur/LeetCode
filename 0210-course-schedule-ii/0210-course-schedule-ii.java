3bd88b521f2f13453d24308ac360c610d9a378fd
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-22 15:25:53 +0530

    Time: 2 ms (99.79%), Space: 45.7 MB (87.94%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,31 +1,39 @@
 class Solution {
-    public int[] findOrder(int n, int[][] preq) {
-        int[] indeg = new int[n];
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0;i<n;i++) adj.add(new ArrayList<>());
-        for(int[] arr:preq){
-            adj.get(arr[1]).add(arr[0]);
-            indeg[arr[0]]++;
+    static{
+        for(int i = 0; i < 1000; i++){
+            findOrder(0, new int[][]{});
         }
-        int[] ans = new int[n];
+    }
+    public static int[] findOrder(int numCourses, int[][] prerequisites) {
+         List<List<Integer>> graph = new ArrayList<>();
+        int course[]=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int pre[]:prerequisites){
+            int sub = pre[0];
+            int precourse = pre[1];
+            graph.get(precourse).add(sub);
+            course[sub]++;
+        }
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            if(indeg[i]==0){
-                q.offer(i);
+        for(int i=0;i<numCourses;i++){
+            if(course[i] == 0){
+                q.add(i);
             }
         }
-        int it = 0;
+        int ans[]=new int[numCourses];
+        int i =0;
         while(!q.isEmpty()){
-            int curr = q.poll();
-            ans[it++] = curr;
-            for(int node : adj.get(curr)){
-                indeg[node]--;
-                if(indeg[node]==0){
-                    q.offer(node);
+            int current = q.poll();
+            ans[i++] = current;
+            for(int neighbor:graph.get(current)){
+                course[neighbor]--;
+                if(course[neighbor] == 0){
+                    q.add(neighbor);
                 }
             }
         }
-        return it==n ? ans : new int[]{}; 
-        
+        return i == numCourses?ans:new int[0];
     }
 }
