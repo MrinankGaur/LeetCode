ee4e8a55594290fa0125b6d8908d25cdac199d6d
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-25 16:39:20 +0530

    Time: 5 ms (99.9%), Space: 56.7 MB (54.12%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,31 +1,39 @@
 class Solution {
-    public int numEnclaves(int[][] grid) {
-        int sum = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if ((i == 0 || j == 0 || i == grid.length - 1 || j == grid[0].length - 1) && grid[i][j] == 1) {
-                    dfs(grid, i, j);
-                }
-            }
-        }
-
-        for (int i[] : grid) {
-            for (int j : i) {
-                sum += j;
-            }
-        }
-        return sum;
-    }
-
-    public void dfs(int g[][], int i, int j) {
-        if (i < 0 || j < 0 || i >= g.length || j >= g[0].length || g[i][j] == 0) {
+    public void dfs(int[][] grid,int i,int j) {
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0) {
             return;
         }
-        g[i][j] = 0;
-        dfs(g, i + 1, j);
-        dfs(g, i, j + 1);
-        dfs(g, i - 1, j);
-        dfs(g, i, j - 1);
-
+        grid[i][j] = 0;
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+    }
+    public int numEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int i=0;i<n;i++) {
+            if(grid[i][0]==1) {
+                dfs(grid,i,0);
+            }
+            if(grid[i][m-1]==1) {
+                dfs(grid,i,m-1);
+            }
+        }
+        for(int i=0;i<m;i++) {
+            if(grid[0][i]==1) {
+                dfs(grid,0,i);
+            }
+            if(grid[n-1][i]==1) {
+                dfs(grid,n-1,i);
+            }
+        }
+        int ans = 0;
+        for(var ele : grid) {
+            for(var e : ele) {
+                ans+=e;
+            }
+        }
+        return ans;
     }
 }
