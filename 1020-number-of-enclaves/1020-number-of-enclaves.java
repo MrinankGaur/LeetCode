b602b4d6183180819733ee3aee6d39f163e130db
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-25 16:38:51 +0530

    Time: 6 ms (98.9%), Space: 57.2 MB (47.25%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,37 +1,31 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] arr = {{0,1},{0,-1},{1,0},{-1,0}};
-        int[][] vis = new int[n][m];
-        for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(i==0 || j==0 || i==n-1 || j==m-1){
-                    if(grid[i][j]==1){
-                        dfs(i,j,n,m,arr,vis,grid);
-                    }
+        int sum = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if ((i == 0 || j == 0 || i == grid.length - 1 || j == grid[0].length - 1) && grid[i][j] == 1) {
+                    dfs(grid, i, j);
                 }
             }
         }
-        int count = 0;
-        for(int i = 0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]==0 && grid[i][j]==1){
-                    count++;
-                }
-            }
-        }
-        return count;
 
-    }
-    public void dfs(int i,int j,int n,int m,int[][] arr,int[][] vis,int[][] board){
-        vis[i][j] = 1;
-        for(int[] dir:arr){
-            int x = i+dir[0];
-            int y = j+dir[1];
-            if(x>=0 && y>=0 && x<n && y<m && board[x][y]==1 && vis[x][y]==0){
-                dfs(x,y,n,m,arr,vis,board);
+        for (int i[] : grid) {
+            for (int j : i) {
+                sum += j;
             }
         }
+        return sum;
+    }
+
+    public void dfs(int g[][], int i, int j) {
+        if (i < 0 || j < 0 || i >= g.length || j >= g[0].length || g[i][j] == 0) {
+            return;
+        }
+        g[i][j] = 0;
+        dfs(g, i + 1, j);
+        dfs(g, i, j + 1);
+        dfs(g, i - 1, j);
+        dfs(g, i, j - 1);
+
     }
 }
