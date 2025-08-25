9ff7cd6ce637ebd930dec8d6737ea3210a65fffd
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-25 16:36:57 +0530

    Time: 10 ms (28.08%), Space: 62.5 MB (5.98%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,42 +1,37 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        Queue<int[]> q = new LinkedList<>();
-        int row = grid.length;
-        int col = grid[0].length;
-        int[][] vis = new int[row][col];
-        for(int i = 0 ;i<row;i++){
-            for(int j = 0 ;j<col;j++){
-                if(i==0 || j==0 || i == row-1 || j== col-1){
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] arr = {{0,1},{0,-1},{1,0},{-1,0}};
+        int[][] vis = new int[n][m];
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
                     if(grid[i][j]==1){
-                        q.offer(new int[]{i,j});
-                        vis[i][j]=1;
+                        dfs(i,j,n,m,arr,vis,grid);
                     }
                 }
             }
         }
-        int[] drow = {0,0,-1,1};
-        int[] dcol = {1,-1,0,0};
-        while(!q.isEmpty()){
-            int r = q.peek()[0];
-            int c = q.peek()[1];
-            q.remove();
-            for(int i =0;i<4;i++){
-                int nr = r + drow[i];
-                int nc = c + dcol[i];
-                if(nr>=0 && nc>=0 && nr<row-1 && nc<col-1 && vis[nr][nc]==0 && grid[nr][nc]==1){
-                    q.offer(new int[]{nr,nc});
-                    vis[nr][nc]=1;
-                }
-            }
-        }
-        int count  = 0;
-        for(int i = 0 ;i<row;i++){
-            for(int j = 0 ;j<col;j++){
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(vis[i][j]==0 && grid[i][j]==1){
-                   count++;
+                    count++;
                 }
             }
         }
         return count;
+
+    }
+    public void dfs(int i,int j,int n,int m,int[][] arr,int[][] vis,int[][] board){
+        vis[i][j] = 1;
+        for(int[] dir:arr){
+            int x = i+dir[0];
+            int y = j+dir[1];
+            if(x>=0 && y>=0 && x<n && y<m && board[x][y]==1 && vis[x][y]==0){
+                dfs(x,y,n,m,arr,vis,board);
+            }
+        }
     }
 }
