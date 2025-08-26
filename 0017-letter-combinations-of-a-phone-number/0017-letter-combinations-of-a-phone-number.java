f869ed435a4eaf22b7027acd72f55a460b0fff33
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-26 19:23:02 +0530

    Time: 0 ms (100%), Space: 42.2 MB (64.99%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,34 +1,37 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-       List<String> res = new ArrayList<>();
-       if(digits==null || digits.length()==0){
-        return res;
-       }
-       Map<Character,String> map = new HashMap<>();
-       map.put('2',"abc");
-       map.put('3',"def");
-       map.put('4',"ghi");
-       map.put('5',"jkl");
-       map.put('6',"mno");
-       map.put('7',"pqrs");
-       map.put('8',"tuv");
-       map.put('9',"wxyz");
-       bt(digits,0,new StringBuilder(),res,map);
-       return res;
-
+        List<String> res = new ArrayList<>();
+        
+        if (digits == null || digits.length() == 0) {
+            return res;
+        }
+        
+        Map<Character, String> digitToLetters = new HashMap<>();
+        digitToLetters.put('2', "abc");
+        digitToLetters.put('3', "def");
+        digitToLetters.put('4', "ghi");
+        digitToLetters.put('5', "jkl");
+        digitToLetters.put('6', "mno");
+        digitToLetters.put('7', "pqrs");
+        digitToLetters.put('8', "tuv");
+        digitToLetters.put('9', "wxyz");
+        
+        backtrack(digits, 0, new StringBuilder(), res, digitToLetters);
+        
+        return res;        
     }
 
-    private void bt(String digits, int idx,StringBuilder comb, List<String> result, Map<Character,String> map){
-        if(idx==digits.length()){
-            result.add(comb.toString());
+    private void backtrack(String digits, int idx, StringBuilder comb, List<String> res, Map<Character, String> digitToLetters) {
+        if (idx == digits.length()) {
+            res.add(comb.toString());
             return;
         }
-
-        String lts = map.get(digits.charAt(idx));
-        for(char it : lts.toCharArray()){
-            comb.append(it);
-            bt(digits,idx+1,comb,result,map);
-            comb.deleteCharAt(comb.length()-1);
+        
+        String letters = digitToLetters.get(digits.charAt(idx));
+        for (char letter : letters.toCharArray()) {
+            comb.append(letter);
+            backtrack(digits, idx + 1, comb, res, digitToLetters);
+            comb.deleteCharAt(comb.length() - 1);
         }
-    }
+    }    
 }
