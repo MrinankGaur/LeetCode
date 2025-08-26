b9db4ffcbce36e02ea789d42c4b9776db694ffb0
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-08-26 14:44:05 +0530

    Time: 63 ms (87.24%), Space: 46.6 MB (59.52%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,39 +1,38 @@
 class Solution {
-    class Pair{
-        String first;
-        int second;
-        Pair(String first,int second){
-            this.first=first;
-            this.second=second;
-        }
-    }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> s=new HashSet<>(wordList);
-        if (!s.contains(endWord)) return 0;
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(beginWord,1));
-
+        Set<String> set = new HashSet<>(wordList);
+        if(!set.contains(endWord)) return 0;
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(beginWord, 1));
         while(!q.isEmpty()){
-            Pair curr=q.poll();
-            String word=curr.first;
-            int step=curr.second;
-
-            char[] wordCharArray=word.toCharArray();
+            Pair curr =  q.poll();
+            String word = curr.str;
+            int step = curr.step;
             if(word.equals(endWord)) return step;
-            for(int i=0;i<word.length();i++){
-                char originalChar=wordCharArray[i];
-                for(char ch='a';ch<='z';ch++){
-                    if (ch == originalChar) continue;
-                    wordCharArray[i]=ch;
-                    String newWord=new String(wordCharArray);
-                    if(s.contains(newWord)){
-                        s.remove(newWord);
-                        q.add(new Pair(newWord,step+1));
+            char[] arr = word.toCharArray();
+            for(int i = 0;i<arr.length;i++){
+                char w = arr[i];
+                for(char ch = 'a';ch <= 'z'; ch++){
+                    if(ch==w) continue;
+                    arr[i] = ch;
+                    String newWord = new String(arr);
+                    if(set.contains(newWord)){
+                        set.remove(newWord);
+                        q.offer(new Pair(newWord,step+1));
                     }
                 }
-                wordCharArray[i] = originalChar;
+                arr[i] = w;
             }
         }
         return 0;
+    }
+
+    public class Pair{
+        String str;
+        int step;
+        public Pair(String str, int step){
+            this.str = str;
+            this.step = step;
+        }
     }
 }
