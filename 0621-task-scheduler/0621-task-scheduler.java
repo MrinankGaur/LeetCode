2992f62ed424c0d707e4f51b67644822e84bb7fb
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-09-01 19:31:00 +0530

    Time: 2 ms (98.73%), Space: 45.4 MB (89.66%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -2,54 +2,25 @@ import java.util.*;
 
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        Map<Character,Integer> map = new HashMap<>();
+        int[] freq = new int[26];
         for (char c : tasks) {
-            map.put(c, map.getOrDefault(c, 0) + 1);          
+            freq[c - 'A']++;
         }
 
+        Arrays.sort(freq);
+        int maxFreq = freq[25];
+        int countMax = 0;
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.freq - a.freq);
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            pq.offer(new Pair(entry.getKey(), entry.getValue()));
+        // Count how many tasks have frequency = maxFreq
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == maxFreq) countMax++;
+            else break;
         }
 
-        Queue<PairCooldown> cooldown = new LinkedList<>();
-        int time = 0;
+        int partCount = maxFreq - 1;
+        int partLength = n + 1;
+        int minTime = partCount * partLength + countMax;
 
-        while (!pq.isEmpty() || !cooldown.isEmpty()) {
-            time++;
-
-            if (!pq.isEmpty()) {
-                Pair curr = pq.poll();
-                curr.freq--;
-                if (curr.freq > 0) {
-                    cooldown.offer(new PairCooldown(curr, time + n));
-                }
-            }
-
-            if (!cooldown.isEmpty() && cooldown.peek().readyTime == time) {
-                pq.offer(cooldown.poll().pair);
-            }
-        }
-
-        return time;
-    }
-
-    class Pair {
-        char task;
-        int freq;
-        public Pair(char task, int freq) {
-            this.task = task;
-            this.freq = freq;
-        }
-    }
-
-    class PairCooldown {
-        Pair pair;
-        int readyTime;
-        public PairCooldown(Pair pair, int readyTime) {
-            this.pair = pair;
-            this.readyTime = readyTime;
-        }
+        return Math.max(tasks.length, minTime);
     }
 }
