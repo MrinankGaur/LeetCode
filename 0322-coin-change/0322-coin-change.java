b263cc5df602073d94f1ab603aceb3b64a60b533
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-09-23 10:04:22 +0530

    Time: 29 ms (23.15%), Space: 45.3 MB (21.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,15 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp=new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0]=0;
-        for(int i=1;i<=amount;i++){
-            for(int coin:coins){
-                if(coin<=i){
-                    dp[i]=Math.min(dp[i],dp[i-coin]+1);
-                }
-            }
-        } 
-        return dp[amount]>amount ? -1 : dp[amount];
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        int ans = helper(n-1,amount,coins,dp);
+        return (ans==(int)1e9)?-1:ans;
+    }
+    public int helper(int i,int target,int[] coins,int[][] dp){
+        if(i==0){
+            if(target%coins[0]==0) return target/coins[0];
+            return (int)1e9;
+
+        }
+        if(dp[i][target]!=-1) return dp[i][target];
+        int notTake = helper(i-1,target,coins,dp);
+        int take = (int)1e9;
+        if(coins[i]<=target) take = 1 + helper(i,target-coins[i],coins,dp);
+        return dp[i][target] = Math.min(notTake,take);
     }
 }
