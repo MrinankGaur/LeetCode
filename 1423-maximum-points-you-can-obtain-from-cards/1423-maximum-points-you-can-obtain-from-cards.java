5e460563cbccb649170f53040c9cb05433045917
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2025-10-08 17:03:32 +0530

    Time: 2 ms (98.62%), Space: 54.9 MB (91.62%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,23 +1,22 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n = cardPoints.length;
-        int totalSum = 0;
-        for (int x : cardPoints) totalSum += x;
-
-        int windowSize = n - k;
-        if (windowSize == 0) return totalSum;
-
+        int window = n - k;
+        int sum = 0;
+        for (int s : cardPoints) sum += s;
+        if (window == 0) return sum;
         int windowSum = 0;
-        for (int i = 0; i < windowSize; i++) {
+        for (int i = 0; i < window; i++) {
             windowSum += cardPoints[i];
         }
 
         int minWindowSum = windowSum;
-        for (int i = windowSize; i < n; i++) {
-            windowSum += cardPoints[i] - cardPoints[i - windowSize];
+
+        for (int i = window; i < n; i++) {
+            windowSum += cardPoints[i] - cardPoints[i - window];
             minWindowSum = Math.min(minWindowSum, windowSum);
         }
 
-        return totalSum - minWindowSum;
+        return sum - minWindowSum;
     }
 }
