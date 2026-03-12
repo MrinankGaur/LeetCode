4ee9e4a3fcbcf0f36d2c08066f98f2ee7944dd80
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-03-12 22:52:20 +0530

    Time: 12 ms (91.26%), Space: 52 MB (49.72%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
@@ -1,52 +1,77 @@
 class Solution {
+
     private class Pair{
         int first;
-        int second;
-        public Pair(int first,int second){
+        long second;
+
+        public Pair(int first,long second){
             this.first = first;
             this.second = second;
         }
     }
 
     public int countPaths(int n, int[][] roads) {
+
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+
         for(int i = 0;i<n;i++){
             adj.add(new ArrayList<>());
         }
+
         for(int[] arr:roads){
             int u = arr[0];
             int v = arr[1];
             int w = arr[2];
+
             adj.get(u).add(new Pair(v,w));
             adj.get(v).add(new Pair(u,w));
         }
+
         long[] dist = new long[n];
-        Arrays.fill(dist,(int)1e9);
-        dist[0]=0;
+        Arrays.fill(dist, Long.MAX_VALUE);
+
         long[] ways = new long[n];
-        ways[0]=1;
+
+        dist[0] = 0;
+        ways[0] = 1;
+
         int mod = (int)1e9 + 7;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Long.compare(a.second, b.second));
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Long.compare(a.second,b.second));
+
         pq.offer(new Pair(0,0));
+
         while(!pq.isEmpty()){
+
             Pair curr = pq.poll();
+
             int node = curr.first;
-            int weight = curr.second;
-            if (weight > dist[node]) continue;
-            for(Pair it: adj.get(node)){
+            long weight = curr.second;
+
+            if(weight > dist[node]) continue;
+
+            for(Pair it : adj.get(node)){
+
                 int next = it.first;
-                int w = it.second + weight;
-                if(w<dist[next]){
-                    dist[next] = w;
-                    pq.offer(new Pair(next, w));
-                    ways[next] = ways[node] % mod;
+                long newDist = weight + it.second;
+
+                if(newDist < dist[next]){
+
+                    dist[next] = newDist;
+                    pq.offer(new Pair(next,newDist));
+                    ways[next] = ways[node];
+
                 }
-                else if(w==dist[next]){
-                    ways[next] = (ways[node]+ways[next])%mod;
+
+                else if(newDist == dist[next]){
+
+                    ways[next] = (ways[next] + ways[node]) % mod;
+
                 }
+
             }
         }
-        return (int)(ways[n - 1] % mod);
 
+        return (int)(ways[n-1] % mod);
     }
 }
