50c43ccef4ee22bf2cdf57644e178bc12827836a
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-03-26 13:41:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i =0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(grid[i][j]==1){
-                    max = Math.max(dfs(grid,i,j),max);
+                    max = Math.max(bfs(grid,n,m,i,j),max);
                 }
             }
         }
