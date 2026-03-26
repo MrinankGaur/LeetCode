17bf9c3b6cf14ce0dcfbdeb48b67cb44ecf1cb7a
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-03-26 13:43:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -8,6 +8,7 @@ class Solution {
             for(int j = 0;j<m;j++){
                 if(grid[i][j]==1){
                     max = Math.max(bfs(grid,n,m,i,j),max);
+                    //max = Math.max(dfs(grid,i,j),max);
                 }
             }
         }
@@ -28,7 +29,6 @@ class Solution {
     }
     public int bfs(int[][] grid,int row, int col,int i,int j){
         Queue<int[]> q = new LinkedList<>();
-        int[][] dir = {{1,0},{-1,0},{0,-1},{0,1}};
         q.offer(new int[]{i,j});
         int size = 1;
         grid[i][j]=0;
@@ -36,7 +36,7 @@ class Solution {
             int[] curr = q.poll();
             int r = curr[0];
             int c = curr[1];
-            for(int[] arr: dir){
+            for(int[] arr: directions){
                 int dr = r + arr[0];
                 int dc = c + arr[1];
                 if(dr>=0 && dc>=0 && dr<row && dc<col && grid[dr][dc]==1){
