6876dd31ca31b4ee35225b891f83cc4f79c6ac2a
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-03-26 13:26:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,33 +2,41 @@ class Solution {
     public int numIslands(char[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
-        int[][] vis = new int[row][col];
-        int count=0;
-        for(int i =0;i<row;i++){
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        int count = 0;
+        for(int i = 0;i<row;i++){
             for(int j = 0;j<col;j++){
-                if(vis[i][j]==0 && grid[i][j]=='1'){
-                    dfs(grid,row,col,i,j,vis);
+                if(grid[i][j]=='1'){
+                    bfs(grid,dir,row,col,i,j);
                     count++;
                 }
             }
         }
         return count;
+        
     }
-    public void dfs(char[][] grid, int r,int c,int i,int j,int[][] vis){
-        if(i<0 || j<0 || i>=r || j>=c){
-            return;
+    public void bfs(char[][] grid, int[][] dir, int row, int col,int i,int j){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{i,j});
+        grid[i][j]='0';
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+            for(int k =0;k<4;k++){
+                int dr = r + dir[k][0];
+                int dc = c + dir[k][1];
+                if(isValid(dr,dc,row,col) && grid[dr][dc]=='1'){
+                    q.offer(new int[]{dr,dc});
+                    grid[dr][dc]='0';
+                }
+            }
         }
-        if(vis[i][j]==1){
-            return;
-        }
-        if(grid[i][j]=='0'){
-            return;
-        }
-        vis[i][j]=1;
-        dfs(grid,r,c,i+1,j,vis);
-        dfs(grid,r,c,i-1,j,vis);
-        dfs(grid,r,c,i,j+1,vis);
-        dfs(grid,r,c,i,j-1,vis);
+
+    }
+    public boolean isValid(int dr, int dc, int row, int col){
+        if(dr>=0 && dc>=0 && dr<row && dc<col) return true;
+        return false;
     }
 
 }
