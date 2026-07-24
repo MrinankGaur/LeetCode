64dd5aa0f9225251fd75e3857c97da2626b399f6
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-07-24 10:44:23 +0530

    Time: 2 ms (93.96%), Space: 72.2 MB (28.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -6,7 +6,6 @@ class Solution {
         for(int i = 1;i<n;i++){
             res[i] = res[i-1]*nums[i-1];
         }
-        System.out.println(Arrays.toString(res));
         int postfix = 1;
         for(int i = n-1;i>=0;i--){
             res[i] *=postfix;
