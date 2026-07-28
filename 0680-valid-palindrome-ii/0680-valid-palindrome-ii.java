e22696bb0ff5de83295b955d13364fcb56037292
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-07-28 14:05:04 +0530

    Time: 4 ms (99.07%), Space: 47.6 MB (67.73%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean validPalindrome(String s) {
         int l = 0;
         int r = s.length()-1;
-        int count = 1;
         while(l<r){
             if(s.charAt(l)!=s.charAt(r)){
                 return isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1);
