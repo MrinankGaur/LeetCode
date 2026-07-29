5235192d7ed968ea883373a53c7663b8ffd46025
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-07-29 10:28:59 +0530

    Time: 28 ms (97.04%), Space: 59.1 MB (60.07%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,28 @@
-public class Solution{
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
         Arrays.sort(nums);
-        for (int i = 0; i < n; i++) {
-            // skip duplicate i
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // skip duplicate left
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    // skip duplicate right
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+        int n = nums.length;
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0;i<n;i++){
+            if(nums[i]>0) break;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int l = i+1,r=n-1;
+            while(l<r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum>0){
+                    r--;
+                }else if(sum<0){
+                    l++;
+                }else{
+                    list.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1]){
+                        l++;
+                    }
                 }
             }
         }
-        return ans;
+        return list;
     }
 }
