edf65838126aa6e84e5037fb80ac5f94fbb584f3
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-07-31 16:08:28 +0530

    Time: 7 ms (69.37%), Space: 43.9 MB (85.78%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,26 +2,26 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int n1 = s1.length();
         int n2 = s2.length();
-
-        if(n2 < n1) return false;
-
-        int[] count1 = new int[26];
-        int[] count2 = new int[26];
-
-        for(int i = 0; i < n1; i++){
-            count1[s1.charAt(i) - 'a']++;
-            count2[s2.charAt(i) - 'a']++;
+        if(n1>n2) return false;
+        int[] map1 = new int[26];
+        int[] map2 = new int[26];
+        for(int i = 0;i<n1;i++){
+            map1[s1.charAt(i)-'a']++;
+            map2[s2.charAt(i)-'a']++;
         }
-
-        if(Arrays.equals(count1, count2)) return true;
-
-        for(int i = n1; i < n2; i++){
-            count2[s2.charAt(i) - 'a']++;                 
-            count2[s2.charAt(i - n1) - 'a']--;          
-
-            if(Arrays.equals(count1, count2)) return true;
+        if(equal(map1,map2,26)) return true;
+        for(int i = n1;i<n2;i++){
+            map2[s2.charAt(i)-'a']++;
+            map2[s2.charAt(i-n1)-'a']--;
+            if(equal(map1,map2,26)) return true;
         }
+        return equal(map1,map2,26);
 
-        return false;
+    }
+    public boolean equal(int[] arr1,int[] arr2,int n){
+        for(int i =0;i<n;i++){
+            if(arr1[i]!=arr2[i]) return false;
+        }
+        return true;
     }
 }
