39723159a1f619958d883c583d8bbeefca6ccd4d
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-07-31 16:00:20 +0530

    Time: 73 ms (14.51%), Space: 46.8 MB (21.11%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,46 +1,28 @@
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) {
-            return false;
+        int l = s1.length();
+        int n = s2.length();
+        if(l>n) return false;
+        int[] map1 = new int[26];
+        for(char ch:s1.toCharArray()){
+            map1[ch-'a']++;
         }
-
-        int[] s1Count = new int[26];
-        int[] s2Count = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            s1Count[s1.charAt(i) - 'a']++;
-            s2Count[s2.charAt(i) - 'a']++;
+        for(int i =0;i<=n-l;i++){
+            int[] map2 = new int[26];
+            for(int j = i;j<i+l;j++){
+                char ch = s2.charAt(j);
+                map2[ch-'a']++;
+            }
+            if(matches(map1,map2,26)) return true;
+            
         }
-
-        int matches = 0;
-        for (int i = 0; i < 26; i++) {
-            if (s1Count[i] == s2Count[i]) {
-                matches++;
-            }
+        return false;
+        
+    }
+    public boolean matches(int[] arr1, int[] arr2, int n){
+        for(int i=0;i<n;i++){
+            if(arr1[i]!=arr2[i]) return false;
         }
-
-        int l = 0;
-        for (int r = s1.length(); r < s2.length(); r++) {
-            if (matches == 26) {
-                return true;
-            }
-
-            int index = s2.charAt(r) - 'a';
-            s2Count[index]++;
-            if (s1Count[index] == s2Count[index]) {
-                matches++;
-            } else if (s1Count[index] + 1 == s2Count[index]) {
-                matches--;
-            }
-
-            index = s2.charAt(l) - 'a';
-            s2Count[index]--;
-            if (s1Count[index] == s2Count[index]) {
-                matches++;
-            } else if (s1Count[index] - 1 == s2Count[index]) {
-                matches--;
-            }
-            l++;
-        }
-        return matches == 26;
+        return true;
     }
 }
