47726b8d44346f840a9ea290858113d46a4eb4c4
Mrinank Gaur <131591761+MrinankGaur@users.noreply.github.com>
2026-09-08 16:11:52 +0530

    Time: 59 ms (78.17%), Space: 103.1 MB (94.92%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,16 +1,28 @@
 public class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] res = new int[temperatures.length];
-        Stack<int[]> stack = new Stack<>(); // pair: [temp, index]
-
-        for (int i = 0; i < temperatures.length; i++) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Stack<int[]> st = new Stack<>();
+        for(int i = 0;i<n;i++){
             int t = temperatures[i];
-            while (!stack.isEmpty() && t > stack.peek()[0]) {
-                int[] pair = stack.pop();
-                res[pair[1]] = i - pair[1];
+            while(!st.isEmpty() && t>st.peek()[0]){
+                int[] curr = st.pop();
+                result[curr[1]] = i-curr[1];
             }
-            stack.push(new int[]{t, i});
+            st.push(new int[]{t,i});
         }
-        return res;
+        return result;
+        // int[] res = new int[temperatures.length];
+        // Stack<int[]> stack = new Stack<>(); // pair: [temp, index]
+
+        // for (int i = 0; i < temperatures.length; i++) {
+        //     int t = temperatures[i];
+        //     while (!stack.isEmpty() && t > stack.peek()[0]) {
+        //         int[] pair = stack.pop();
+        //         res[pair[1]] = i - pair[1];
+        //     }
+        //     stack.push(new int[]{t, i});
+        // }
+        // return res;
     }
 }
